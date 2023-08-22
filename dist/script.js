718e7d4a3fc27f7ef5f03e8d328a1f618d38de3b
HeatGub <ptrchm@proton.me>
2023-08-22 18:58:36 +0200

    Primitive tree rising

--- dist/script.js
+++ dist/script.js
@@ -6,9 +6,8 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d');
     canvas.width = 600;
     canvas.height = 800;
-    ctx.strokeStyle = 'brown';
     class Branch {
-        constructor(x0, y0, len, angle, lineWidth = 16, //trunk width
+        constructor(x0, y0, len, angle, lineWidth = 25, //trunk width
         xF = 0, //could be ? but then lineTo errors with null
         yF = 0, level = 0, parent, // parent branch
         children = []) {
@@ -26,27 +25,54 @@ window.addEventListener('load', function () {
             this.xF = x0 + Math.sin(angle / 180 * Math.PI) * len;
             this.yF = y0 - Math.cos(angle / 180 * Math.PI) * len;
         }
-        makeChildrenBranch(angleDiff) {
-            ctx.strokeStyle = 'green'; // why is the trunk green?
+        makeChildBranch(angleDiff) {
+            // ctx.strokeStyle = 'green' // why is the trunk green?
             let childBranch = new Branch(this.xF, this.yF, this.len * 0.8, angleDiff, this.lineWidth * 0.7);
             childBranch.level = this.level + 1;
             childBranch.parent = this;
+            // childBranch.parent.angle += angleDiff
             this.children.push(childBranch);
-            console.log(childBranch);
-            this.drawBranch(childBranch);
+            // console.log(childBranch)
+            this.drawBranch();
+            return childBranch;
         }
-        drawBranch(object) {
-            ctx.lineWidth = object.lineWidth;
-            ctx.moveTo(object.x0, object.y0);
-            ctx.lineTo(object.xF, object.yF);
+        drawBranch() {
+            ctx.lineWidth = this.lineWidth;
+            ctx.strokeStyle = 'rgb(10,' + (80 + 60 * this.level) + ', 0)';
+            ctx.moveTo(this.x0, this.y0);
+            ctx.lineTo(this.xF, this.yF);
+            console.log(this.level);
+            // console.log(this.lineWidth)
             ctx.stroke();
         }
     }
-    let branch = new Branch(canvas.width / 2, canvas.height, 200, 0);
-    console.log(branch);
-    branch.drawBranch(branch);
-    branch.makeChildrenBranch(10);
-    branch.makeChildrenBranch(-10);
+    class Tree {
+        constructor(initX, initY, initLen, initAngle, maxLevel = 5, trunk = new Branch(initX, initY, initLen, initAngle), //trunk as branch
+        everyLevelBranches = [[]]) {
+            this.initX = initX;
+            this.initY = initY;
+            this.initLen = initLen;
+            this.initAngle = initAngle;
+            this.maxLevel = maxLevel;
+            this.trunk = trunk;
+            this.everyLevelBranches = everyLevelBranches;
+            //save trunk as 0lvl branch
+            this.everyLevelBranches[0] = [this.trunk];
+            // let currLvl = 1 -> LOOP ALL LEVELS EXCEPT 0 (TRUNK)
+            for (let currLvl = 1; currLvl < this.maxLevel; currLvl++) {
+                this.everyLevelBranches.push([]); // push empty array to fill it by the forEach loop
+                this.everyLevelBranches[currLvl - 1].forEach(element => {
+                    console.log('parent = ' + element);
+                    this.everyLevelBranches[currLvl].push(element.makeChildBranch(45));
+                    this.everyLevelBranches[currLvl].push(element.makeChildBranch(-45));
+                });
+            }
+        } // constructor end
+    }
+    const tree = new Tree(canvas.width / 2, canvas.height, 160, 0); // initialize tree with trunk params
+    // tree.trunk.drawBranch() //remove later?
+    console.log(tree.everyLevelBranches);
+    // console.log(tree.levelBranchesLists[0][0])
 });
 // ______________ CURRENT ______________
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -5,7 +5,6 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
     canvas.width = 600
     canvas.height = 800
-    ctx.strokeStyle = 'brown'
 
     class Branch {
         constructor(
@@ -13,9 +12,9 @@ window.addEventListener('load', function() {
             public y0: number,
             public len: number,
             public angle: number,
-            public lineWidth: number = 16, //trunk width
-            public xF: number = 0, //could be ? but then lineTo errors with null
-            public yF: number  = 0,
+            private lineWidth: number = 25, //trunk width
+            private xF: number = 0, //could be ? but then lineTo errors with null
+            private yF: number  = 0,
             public level: number = 0,
             public parent?: Branch, // parent branch
             public children: Branch[] = [], // list of children branches
@@ -25,30 +24,60 @@ window.addEventListener('load', function() {
             this.yF = y0 - Math.cos(angle/180* Math.PI) * len
         }
 
-        makeChildrenBranch(angleDiff: number) {
-            ctx.strokeStyle = 'green' // why is the trunk green?
+        makeChildBranch(angleDiff: number) {
+            // ctx.strokeStyle = 'green' // why is the trunk green?
             let childBranch = new Branch (this.xF, this.yF, this.len*0.8, angleDiff, this.lineWidth*0.7)
             childBranch.level = this.level +1
             childBranch.parent = this
+            // childBranch.parent.angle += angleDiff
             this.children.push(childBranch)
-            console.log(childBranch)
-            this.drawBranch(childBranch)
+            // console.log(childBranch)
+            this.drawBranch()
+            return childBranch
         }
 
-        drawBranch(object: Branch) {
-            ctx.lineWidth = object.lineWidth
-            ctx.moveTo(object.x0, object.y0)
-            ctx.lineTo(object.xF, object.yF)
+        drawBranch() {
+            ctx.lineWidth = this.lineWidth
+            ctx.strokeStyle = 'rgb(10,' + (80 + 60*this.level) + ', 0)'
+            ctx.moveTo(this.x0, this.y0)
+            ctx.lineTo(this.xF, this.yF)
+            console.log(this.level)
+            // console.log(this.lineWidth)
             ctx.stroke()
-        } 
-
+        }
     }
 
-    let branch = new Branch (canvas.width/2, canvas.height, 200, 0)
-    console.log(branch)
-    branch.drawBranch(branch)
-    branch.makeChildrenBranch(10)
-    branch.makeChildrenBranch(-10)
+    class Tree {
+        constructor(
+            readonly initX: number,
+            readonly initY: number,
+            readonly initLen: number,
+            readonly initAngle: number,
+            readonly maxLevel: number = 5,
+            readonly trunk: Branch = new Branch (initX, initY, initLen, initAngle), //trunk as branch
+            public everyLevelBranches: [Branch[]] = [[]],
+        ){
+            //save trunk as 0lvl branch
+            this.everyLevelBranches[0] = [this.trunk]
+
+            // let currLvl = 1 -> LOOP ALL LEVELS EXCEPT 0 (TRUNK)
+            for (let currLvl = 1; currLvl < this.maxLevel; currLvl++) {
+                this.everyLevelBranches.push([]) // push empty array to fill it by the forEach loop
+                this.everyLevelBranches[currLvl-1].forEach( element => {
+                    console.log('parent = ' + element)
+                    this.everyLevelBranches[currLvl].push(element.makeChildBranch(45))
+                    this.everyLevelBranches[currLvl].push(element.makeChildBranch(-45))
+                })
+            }
+        }// constructor end
+    }
+
+    const tree = new Tree (canvas.width/2, canvas.height, 160, 0) // initialize tree with trunk params
+    // tree.trunk.drawBranch() //remove later?
+    console.log(tree.everyLevelBranches)
+    // console.log(tree.levelBranchesLists[0][0])
 
 })
+
+
 // ______________ CURRENT ______________
