ad082efda394f7692b0ac2709ceac4237a877fdb
HeatGub <ptrchm@proton.me>
2023-08-27 22:12:58 +0200

    Occasional branching at nodes with level shift

--- src/script.ts
+++ src/script.ts
@@ -3,11 +3,12 @@ window.addEventListener('load', function() {
 const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 
-const segmentingLen = 2
+const segmentingLen = 125
 const trunkLen = 200
 const lenMultiplier = 0.71
 const trunkWidth = 40
-const maxLevelGlobal = 8
+const maxLevelGlobal = 9
+let segAmountByLevel = Math.ceil( (trunkLen* (Math.pow(lenMultiplier, 0)) ) / segmentingLen ) //for a trunk
 
 //  SET CANVAS SIZES AND CHANGE THEM AT WINDOW RESIZE
 canvas.width = window.innerWidth
@@ -44,17 +45,17 @@ class Branch {
         this.yF = this.y0 - Math.cos(this.angle/180* Math.PI) * this.len
 
         // SEGMENTING A BRANCH
-        // let segAmount = Math.ceil(this.len / segmentingLen) //MAY RESULT IN DIFFERENT AMOUNT FOR SAME LEVEL, WHICH 'FREEZES' ANIMATION (because of waiting for the last segments to draw)
-        let segAmount = Math.ceil( (trunkLen* (Math.pow(lenMultiplier, this.level)) ) / segmentingLen ) // now it only depends on globals and level
-        // console.log(segAmount)
-        for (let seg=0; seg < segAmount; seg++){
+        // let segAmountByLevel = Math.ceil(this.len / segmentingLen) //MAY RESULT IN DIFFERENT AMOUNT FOR SAME LEVEL, WHICH 'FREEZES' ANIMATION (because of waiting for the last segments to draw)
+        
+        // console.log(segAmountByLevel)
+        for (let seg=0; seg < segAmountByLevel; seg++){
             this.segments.push({x0: 0, y0: 0, xF: 0, yF: 0})
             // Calculate coordinates analogically to branch xF yF, but for shorter lengths. 
             // segment is in range from (seg/segAmount) to ((seg +1)/segAmount) * len
-            this.segments[seg].x0 = this.x0 + Math.sin(this.angle/180* Math.PI) * this.len * (seg/segAmount)
-            this.segments[seg].y0 = this.y0 - Math.cos(this.angle/180* Math.PI) * this.len * (seg/segAmount)
-            this.segments[seg].xF = this.x0 + Math.sin(this.angle/180* Math.PI) * this.len * ((seg +1)/segAmount)
-            this.segments[seg].yF = this.y0 - Math.cos(this.angle/180* Math.PI) * this.len * ((seg +1)/segAmount)
+            this.segments[seg].x0 = this.x0 + Math.sin(this.angle/180* Math.PI) * this.len * (seg/segAmountByLevel)
+            this.segments[seg].y0 = this.y0 - Math.cos(this.angle/180* Math.PI) * this.len * (seg/segAmountByLevel)
+            this.segments[seg].xF = this.x0 + Math.sin(this.angle/180* Math.PI) * this.len * ((seg +1)/segAmountByLevel)
+            this.segments[seg].yF = this.y0 - Math.cos(this.angle/180* Math.PI) * this.len * ((seg +1)/segAmountByLevel)
         }
     } // Branch constructor
 
@@ -123,12 +124,20 @@ class Tree {
     ){
         const startTime = Date.now()
         this.allBranches[0] = [new Branch (initX, initY, initLen, initAngle, trunkWidth, root)]   //save trunk as 0lvl branch
+        // append array for every level ahead. Needed for levelShifts
+        for (let lvl = 0; lvl < this.maxLevel; lvl++) {
+            this.allBranches.push([]) //
+        }
+        // console.log(this.allBranches)
+
         for (let currLvl = 0; currLvl < this.maxLevel; currLvl++) {
             // prob should = 1 for level 0 (trunk) 
             // this variable lowers branching probability with lever. In range from 1 to branchingProbability linearly
             let branchingProbabilityByLevel = branchingProbability + ( (1-branchingProbability) * ((this.maxLevel-currLvl)/this.maxLevel) )
             // console.log(branchingProbabilityByLevel, currLvl)
-            this.allBranches.push([]) // push empty array to fill it by the forEach loop
+            segAmountByLevel = Math.ceil( (trunkLen* (Math.pow(lenMultiplier, currLvl)) ) / segmentingLen ) // now it only depends on globals and level
+
+            // this.allBranches.push([]) // push empty array to fill it by the forEach loop
             this.allBranches[currLvl].forEach( element => {
                 // MAKE BRANCHES
                 if (Math.random() < branchingProbabilityByLevel){
@@ -136,6 +145,16 @@ class Tree {
                 }
                 if (Math.random() < branchingProbabilityByLevel){
                     this.allBranches[currLvl+1].push(element.makeChildBranch(element,-20 - Math.random()*15))
+                }
+                // OCCASIONAL BRANCHING WITH LEVEL SHIFT
+                if (Math.random() < branchingProbabilityByLevel/4) {
+                    console.log('occasional branching')
+                    const levelShift = Math.round(Math.random()*1)
+                    if (element.level + levelShift < this.maxLevel) {
+                        const occasionalBranch = element.makeChildBranch(element, -20 + Math.random()*40)
+                        occasionalBranch.level += levelShift
+                        this.allBranches[currLvl+1+levelShift].push(occasionalBranch)
+                    }
                 }
             })
         }
