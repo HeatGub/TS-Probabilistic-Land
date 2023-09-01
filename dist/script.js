6d34862c7d4f6b4f6598c9f71d07c774aa7e51a0
HeatGub <ptrchm@proton.me>
2023-09-01 17:39:12 +0200

    First leaf drawn with bezier curves

--- dist/script.js
+++ dist/script.js
@@ -3,15 +3,13 @@ window.addEventListener('load', function () {
     //GLOBALS
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    const segmentingLen = 20;
-    const trunkLen = 250;
-    const trunkWidth = 50;
+    const segmentingLen = 1000;
+    const trunkLen = 200;
+    const trunkWidth = 60;
     const lenMultiplier = 0.71;
     const widthMultiplier = 0.75;
-    const maxLevelGlobal = 12;
-    // const levelShift = 1 + Math.round(Math.random()*1)
-    // let levelShift = 0
-    let segAmountByLevel = Math.ceil((trunkLen * (Math.pow(lenMultiplier, 0))) / segmentingLen); //for a trunk
+    const rebranchingAngle = 23;
+    const maxLevelGlobal = 8;
     //  SET CANVAS SIZES AND CHANGE THEM AT WINDOW RESIZE
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -56,6 +54,7 @@ window.addEventListener('load', function () {
             this.yF = this.y0 - Math.cos(this.angle / 180 * Math.PI) * this.len;
             // SEGMENTING A BRANCH
             // let segAmountByLevel = Math.ceil(this.len / segmentingLen) //MAY RESULT IN DIFFERENT AMOUNT FOR SAME LEVEL, WHICH 'FREEZES' ANIMATION (because of waiting for the last segments to draw)
+            let segAmountByLevel = Math.ceil(((trunkLen * (Math.pow(lenMultiplier, this.level))) / segmentingLen) + (this.level / 2));
             // console.log(segAmountByLevel)
             for (let seg = 0; seg < segAmountByLevel; seg++) {
                 this.segments.push({ x0: 0, y0: 0, xF: 0, yF: 0 });
@@ -98,8 +97,8 @@ window.addEventListener('load', function () {
             const gradient = ctx.createLinearGradient(this.x0, this.y0, this.xF, this.yF);
             // gradient.addColorStop(0, 'rgb(80,' + (10 + 10*this.level) + ', 0)');
             // gradient.addColorStop(1, 'rgb(80,' + (20 + 10*this.level) + ', 0)');
-            gradient.addColorStop(0, 'rgb(80,' + (15 * this.parent.level) + ', 0)');
-            gradient.addColorStop(1, 'rgb(80,' + (15 * this.level) + ', 0)');
+            gradient.addColorStop(0, 'rgb(50,' + (12 * this.parent.level) + ', 0)');
+            gradient.addColorStop(1, 'rgb(50,' + (12 * this.level) + ', 0)');
             // gradient.addColorStop(0, 'rgb(10,' + (10 + 10*this.level) + ',' + (100*this.levelShift) + ')' );
             // gradient.addColorStop(1, 'rgb(10,' + (20 + 10*this.level) + ',' + (100*this.levelShift) + ')' );
             ctx.strokeStyle = gradient;
@@ -124,7 +123,7 @@ window.addEventListener('load', function () {
             this.allBranches = allBranches;
             const startTime = Date.now();
             this.allBranches[0] = [new Branch(root, initX, initY, initLen, initAngle, trunkWidth)]; //save trunk as 0lvl branch
-            // append array for every level ahead. Needed for levelShifts
+            // append array for every level ahead. Needed for levelShifted branches
             for (let lvl = 0; lvl < this.maxLevel; lvl++) {
                 this.allBranches.push([]); //
             }
@@ -133,25 +132,24 @@ window.addEventListener('load', function () {
                 // prob should = 1 for level 0 (trunk) 
                 // this variable lowers branching probability with level. In range from 1 to branchingProbability linearly
                 let branchingProbabilityByLevel = this.branchingProbability + ((1 - branchingProbability) * ((this.maxLevel - currLvl) / this.maxLevel));
-                let occasionalBranchingProbability = ((this.maxLevel - currLvl) / this.maxLevel);
+                let occasionalBranchingProbability = ((this.maxLevel - currLvl + 1) / this.maxLevel); // always spawn at lvl 0
                 // console.log(branchingProbabilityByLevel, currLvl)
-                segAmountByLevel = Math.ceil((trunkLen * (Math.pow(lenMultiplier, currLvl))) / segmentingLen); // now it only depends on globals and level
                 // this.allBranches.push([]) // push empty array to fill it by the forEach loop
                 this.allBranches[currLvl].forEach(element => {
                     // MAKE BRANCHES
                     if (Math.random() < branchingProbabilityByLevel) {
-                        this.allBranches[currLvl + 1].push(element.makeChildBranch(20 + Math.random() * 15, 0));
+                        this.allBranches[currLvl + 1].push(element.makeChildBranch(rebranchingAngle + Math.random() * rebranchingAngle, 0));
                     }
                     if (Math.random() < branchingProbabilityByLevel) {
-                        this.allBranches[currLvl + 1].push(element.makeChildBranch(-20 - Math.random() * 15, 0));
+                        this.allBranches[currLvl + 1].push(element.makeChildBranch(-rebranchingAngle - Math.random() * rebranchingAngle, 0));
                     }
                     // OCCASIONAL BRANCHING WITH LEVEL SHIFT (children level is not parent level + 1)
                     if (Math.random() < occasionalBranchingProbability) {
                         // random level shift
-                        let levelShift = 1 + Math.round(Math.random() * 1);
+                        let levelShift = 1 + Math.round(Math.random() * 2);
                         // console.log('occasional branching')
                         if (element.level + 1 + levelShift < this.maxLevel) {
-                            const occasionalBranch = element.makeChildBranch(-20 + Math.random() * 40, levelShift);
+                            const occasionalBranch = element.makeChildBranch(-rebranchingAngle + Math.random() * 2 * rebranchingAngle, levelShift);
                             this.allBranches[currLvl + 1 + levelShift].push(occasionalBranch);
                             // console.log('occasional, lvl =' + (currLvl+levelShift))
                         }
@@ -178,6 +176,83 @@ window.addEventListener('load', function () {
             this.level = level;
         }
     }
+    class Leaf {
+        constructor(
+        // public parent: Branch, // parent branch
+        x0, y0, len, angle, lineWidth = 2, xF = 0, //could be ? but then lineTo errors with null
+        yF = 0, currentStage = 0, stages = []) {
+            this.x0 = x0;
+            this.y0 = y0;
+            this.len = len;
+            this.angle = angle;
+            this.lineWidth = lineWidth;
+            this.xF = xF;
+            this.yF = yF;
+            this.currentStage = currentStage;
+            this.stages = stages;
+            // this.len = this.len + this.len*Math.random()*0.15
+            ctx.lineWidth = this.lineWidth;
+            // recalculate the angle according to parent branch first 
+            // this.angle = this.parent.angle + this.angle
+            // CALCULATE TIP (FINAL) COORDINATES. LEAF'S MAIN NERVE ENDS HERE
+            this.xF = this.x0 + Math.sin(this.angle / 180 * Math.PI) * this.len;
+            this.yF = this.y0 - Math.cos(this.angle / 180 * Math.PI) * this.len;
+            // AXIS 1 WILL BE THE WIDER ONE. IT IS PERPENDICULAR TO THE LEAF'S MAIN NERVE (x0,y0 - xF,yF)
+            // COORD OF AXIS 1 INTERSECTION WITH MAIN LEAF
+            // ratio of length for a crossing point of the lines (main nerve and axis1)
+            const axis1Width = 300;
+            const axis2Width = 10;
+            const axis1LenRatio = -0.25;
+            const axis2LenRatio = 0.5;
+            const petioleLenRatio = 0.2; //of the whole length
+            // PETIOLE END COORDS
+            let xFPetiole = this.x0 + Math.sin(this.angle / 180 * Math.PI) * this.len * petioleLenRatio;
+            let yFPetiole = this.y0 - Math.cos(this.angle / 180 * Math.PI) * this.len * petioleLenRatio;
+            // AXIS 1
+            let x0A1 = this.x0 + Math.sin(this.angle / 180 * Math.PI) * this.len * axis1LenRatio;
+            let y0A1 = this.y0 - Math.cos(this.angle / 180 * Math.PI) * this.len * axis1LenRatio;
+            // calculate points perpendiuclar to the main nerve
+            let xA1Bez1 = x0A1 + Math.sin((90 + this.angle) / 180 * Math.PI) * axis1Width / 2;
+            let yA1Bez1 = y0A1 - Math.cos((90 + this.angle) / 180 * Math.PI) * axis1Width / 2;
+            let xA1Bez2 = x0A1 + Math.sin((-90 + this.angle) / 180 * Math.PI) * axis1Width / 2;
+            let yA1Bez2 = y0A1 - Math.cos((-90 + this.angle) / 180 * Math.PI) * axis1Width / 2;
+            // AXIS 2
+            let x0A2 = this.x0 + Math.sin(this.angle / 180 * Math.PI) * this.len * axis2LenRatio;
+            let y0A2 = this.y0 - Math.cos(this.angle / 180 * Math.PI) * this.len * axis2LenRatio;
+            // calculate points perpendiuclar to the main nerve
+            let xA2Bez1 = x0A2 + Math.sin((90 + this.angle) / 180 * Math.PI) * axis2Width / 2;
+            let yA2Bez1 = y0A2 - Math.cos((90 + this.angle) / 180 * Math.PI) * axis2Width / 2;
+            let xA2Bez2 = x0A2 + Math.sin((-90 + this.angle) / 180 * Math.PI) * axis2Width / 2;
+            let yA2Bez2 = y0A2 - Math.cos((-90 + this.angle) / 180 * Math.PI) * axis2Width / 2;
+            ctx.beginPath();
+            ctx.strokeStyle = 'green';
+            //MAIN NERVE
+            ctx.moveTo(this.x0, this.y0);
+            ctx.lineTo(this.xF, this.yF);
+            ctx.stroke();
+            ctx.closePath();
+            // ctx.beginPath();
+            // // DRAW BOTH AXES
+            // ctx.moveTo(xA1Bez1, yA1Bez1)
+            // ctx.lineTo(xA1Bez2, yA1Bez2)
+            // ctx.moveTo(xA2Bez1, yA2Bez1)
+            // ctx.lineTo(xA2Bez2, yA2Bez2)
+            ctx.beginPath();
+            ctx.moveTo(xFPetiole, yFPetiole);
+            // right side of a leaf
+            ctx.bezierCurveTo(xA1Bez1, yA1Bez1, xA2Bez1, yA2Bez1, this.xF, this.yF);
+            ctx.moveTo(xFPetiole, yFPetiole);
+            // left side of a leaf
+            ctx.bezierCurveTo(xA1Bez2, yA1Bez2, xA2Bez2, yA2Bez2, this.xF, this.yF);
+            ctx.closePath();
+            ctx.fillStyle = 'rgb(0,150,0)';
+            ctx.fill();
+            ctx.stroke();
+        } //Leaf constructor
+    }
+    const leaf = new Leaf(250, 250, 200, 25, 3);
+    console.log(leaf);
+    // leaf.method()
     // _________ INITIALIZE THE TREE _________
     // Root just acts as a parent element for the trunk. 
     // With the root there is no need for checking for parent element in Branch constructor
@@ -240,5 +315,27 @@ window.addEventListener('load', function () {
     // animate
     animateByLSegments(0);
     // _________ ANIMATE SEGMENTS _________
-});
+    // class Cloud {
+    //     constructor (context: CanvasRenderingContext2D) {
+    //     // begin custom shape
+    //     context.beginPath();
+    //     context.moveTo(170, 80);
+    //     context.bezierCurveTo(130, 100, 130, 150, 230, 150);
+    //     context.bezierCurveTo(250, 180, 320, 180, 340, 150);
+    //     context.bezierCurveTo(420, 150, 420, 120, 390, 100);
+    //     context.bezierCurveTo(430, 40, 370, 30, 340, 50);
+    //     context.bezierCurveTo(320, 5, 250, 20, 250, 50);
+    //     context.bezierCurveTo(200, 5, 150, 20, 170, 80);
+    //     // complete custom shape
+    //     context.closePath();
+    //     context.lineWidth = 5;
+    //     context.fillStyle = '#8ED6FF';
+    //     context.fill();
+    //     context.strokeStyle = 'blue';
+    //     context.stroke();
+    //     }
+    // }
+    // const cloud = new Cloud (ctx)
+    // console.log(cloud)
+}); //window.addEventListener('load', function(){ }) ends here
 //# sourceMappingURL=script.js.map
